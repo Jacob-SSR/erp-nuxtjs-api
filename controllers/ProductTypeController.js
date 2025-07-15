8c4d5756d6d79f0d1a9385d6588cfec5ffb55527
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-07-15 14:56:34 +0700

    crud productType

--- controllers/ProductTypeController.js
+++ controllers/ProductTypeController.js
@@ -11,6 +11,56 @@ exports.create = async (req, res, next) => {
     });
     res.status(201).json({ message: "success" });
   } catch (error) {
-      next(error);
+    next(error);
+  }
+};
+
+exports.list = async (req, res, next) => {
+  try {
+    const productTypes = await prisma.productType.findMany({
+      where: {
+        status: "active",
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.json({ results: productTypes });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, remark } = req.body;
+    await prisma.productType.update({
+      where: {
+        id,
+      },
+      data: {
+        name: name,
+        remark: remark,
+      },
+    });
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await prisma.productType.update({
+      where: { id },
+      data: {
+        status: "inactive",
+      },
+    });
+    res.status(204).json({ message: "success" });
+  } catch (error) {
+    next(error);
   }
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,5 +3,8 @@ const productRouter = express.Router();
 const ProductTypeController = require("../controllers/ProductTypeController");
 
 productRouter.post("/productType/create", ProductTypeController.create);
+productRouter.get("/productType/list", ProductTypeController.list);
+productRouter.put("/productType/update/:id", ProductTypeController.update);
+productRouter.delete("/productType/remove/:id", ProductTypeController.remove);
 
 module.exports = productRouter;
